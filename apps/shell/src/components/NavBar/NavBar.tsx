c3c837d1621801e547fff519b6724ac6f1a23455
Ritesh Jagga <riteshjagga@gmail.com>
2025-12-24 17:34:28 +0530

    SETUP: Use NavLink instead of Link to show an active state in the nav bar

--- apps/shell/src/components/NavBar/NavBar.tsx
+++ apps/shell/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { useAuth } from '@workspace/shared/contexts/auth'
 import { Button } from '@workspace/shared/components/ui/button'
 import { ModeToggle } from '../ModeToggle/ModeToggle'
@@ -15,8 +15,29 @@ export default function NavBar() {
 
   return (
     <nav className="flex items-center gap-4 p-4 bg-background text-foreground">
-      <Link to="/mfe-account/account">Account Management</Link>
-      <Link to="/mfe-underwriting/dashboard">Underwriting Management</Link>
+      <NavLink to="/dashboard">Dashboard</NavLink>
+      <NavLink to="/account">Account</NavLink>
+      <NavLink to="/settings">Settings</NavLink>
+      <NavLink
+        to="/mfe-account/dashboard"
+        className={() => {
+          const pathname = location.pathname // or use useLocation() hook
+          const isActive = pathname.startsWith('/mfe-account')
+          return isActive ? 'active' : '' // Add your active styles
+        }}
+      >
+        Account Management
+      </NavLink>
+      <NavLink
+        to="/mfe-underwriting/dashboard"
+        className={() => {
+          const pathname = location.pathname // or use useLocation() hook
+          const isActive = pathname.startsWith('/mfe-underwriting')
+          return isActive ? 'active' : '' // Add your active styles
+        }}
+      >
+        Underwriting Management
+      </NavLink>
       <ModeToggle />
       <Button onClick={handleSignoutClick} variant="outline">
         Sign out
